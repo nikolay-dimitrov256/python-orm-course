f337f6264083e7a94b8441947d6745be85b31ac5
Nikolay Dimitrov <141030298+nikolay-dimitrov256@users.noreply.github.com>
2024-07-08 08:35:44 +0300

    Update settings.py

--- 01 ORM Introduction/orm_skeleton/settings.py
+++ 01 ORM Introduction/orm_skeleton/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "my-site-database",
         "USER": "postgres",
-        "PASSWORD": "pixizaver",
+        "PASSWORD": "admin",
         "HOST": "localhost",
         "PORT": "5432",
     }
