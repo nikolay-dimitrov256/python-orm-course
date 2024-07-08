9e97082c751ae50cd3219f4f085c37f5ef92bdf1
nikolay_dimitrov256 <raystlin7@gmail.com>
2024-07-08 23:05:42 +0300

    Solutions to tasks

---
+++ 07 Models Inheritance and Customisation Lab/main_app/models.py
@@ -0,0 +1,82 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+
+
+class Animal(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+    species = models.CharField(
+        max_length=100,
+    )
+    birth_date = models.DateField()
+    sound = models.CharField(
+        max_length=100,
+    )
+
+
+class Mammal(Animal):
+    fur_color = models.CharField(
+        max_length=50,
+    )
+
+
+class Bird(Animal):
+    wing_span = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+    )
+
+
+class Reptile(Animal):
+    scale_type = models.CharField(
+        max_length=50,
+    )
+
+
+class Employee(models.Model):
+    class Meta:
+        abstract = True
+
+    first_name = models.CharField(
+        max_length=50,
+    )
+    last_name = models.CharField(
+        max_length=50,
+    )
+    phone_number = models.CharField(
+        max_length=10,
+    )
+
+
+class SpecialtyChoices(models.TextChoices):
+    MAMMALS = 'Mammals', 'Mammals'
+    BIRDS = 'Birds', 'Birds'
+    REPTILES = 'Reptiles', 'Reptiles'
+    OTHERS = 'Others', 'Others'
+
+
+class ZooKeeper(Employee):
+    specialty = models.CharField(
+        max_length=10,
+        choices=SpecialtyChoices.choices,
+    )
+    managed_animals = models.ManyToManyField(
+        to=Animal,
+    )
+
+    def clean(self) -> None:
+        if self.specialty not in SpecialtyChoices:
+            raise ValidationError('Specialty must be a valid choice.')
+
+
+class Veterinarian(Employee):
+    license_number = models.CharField(
+        max_length=10,
+    )
+
+
+class ZooDisplayAnimal(Animal):
+    class Meta:
+        proxy = True
+
