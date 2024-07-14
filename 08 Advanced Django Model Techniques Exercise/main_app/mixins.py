57dba8d08ebf68f66b3bb4d43d86014e41a75ff3
nikolay_dimitrov256 <raystlin7@gmail.com>
2024-07-14 10:29:55 +0300

    Solutions to tasks

---
+++ 08 Advanced Django Model Techniques Exercise/main_app/mixins.py
@@ -0,0 +1,3 @@
+class RechargeEnergyMixin:
+    def recharge_energy(self, amount: int) -> None:
+        self.energy = min(100, self.energy + amount)
--- 08 Advanced Django Model Techniques Exercise/main_app/models.py
+++ 08 Advanced Django Model Techniques Exercise/main_app/models.py
@@ -1,14 +1,19 @@
+from decimal import Decimal
+
+from django.contrib.postgres.search import SearchVectorField
 from django.db import models
-from django.core.validators import MinValueValidator, RegexValidator
+from django.core.validators import MinValueValidator, RegexValidator, MinLengthValidator
+
+from main_app.validators import ValidateName
+from main_app.mixins import RechargeEnergyMixin
 
 # Create your models here.
-from main_app.validators import validate_name
 
 
 class Customer(models.Model):
     name = models.CharField(
         max_length=100,
-        validators=[validate_name]
+        validators=[ValidateName(message='Name can only contain letters and spaces')]
     )
 
     age = models.PositiveIntegerField(
@@ -31,3 +36,166 @@ class Customer(models.Model):
     website_url = models.URLField(
         error_messages={'invalid': 'Enter a valid URL'}
     )
+
+
+class BaseMedia(models.Model):
+    title = models.CharField(
+        max_length=100,
+    )
+
+    description = models.TextField()
+
+    genre = models.CharField(
+        max_length=50,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at', 'title']
+
+
+class Book(BaseMedia):
+    author = models.CharField(
+        max_length=100,
+        validators=[
+            MinLengthValidator(5, message='Author must be at least 5 characters long')
+        ]
+    )
+
+    isbn = models.CharField(
+        max_length=20,
+        unique=True,
+        validators=[
+            MinLengthValidator(6, message='ISBN must be at least 6 characters long')
+        ]
+    )
+
+    class Meta(BaseMedia.Meta):
+        verbose_name = 'Model Book'
+        verbose_name_plural = 'Models of type - Book'
+
+
+class Movie(BaseMedia):
+    director = models.CharField(
+        max_length=100,
+        validators=[
+            MinLengthValidator(8, message='Director must be at least 8 characters long')
+        ]
+    )
+
+    class Meta(BaseMedia.Meta):
+        verbose_name = 'Model Movie'
+        verbose_name_plural = 'Models of type - Movie'
+
+
+class Music(BaseMedia):
+    artist = models.CharField(
+        max_length=100,
+        validators=[
+            MinLengthValidator(9, message='Artist must be at least 9 characters long')
+        ]
+    )
+
+    class Meta(BaseMedia.Meta):
+        verbose_name = 'Model Music'
+        verbose_name_plural = 'Models of type - Music'
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    def calculate_tax(self) -> Decimal:
+        return self.price * Decimal(0.08)
+
+    @staticmethod
+    def calculate_shipping_cost(weight: Decimal) -> Decimal:
+        return weight * Decimal(2)
+
+    def format_product_name(self) -> str:
+        return f'Product: {self.name}'
+
+
+class DiscountedProduct(Product):
+    class Meta:
+        proxy = True
+
+    def calculate_price_without_discount(self) -> Decimal:
+        return self.price * Decimal(1.2)
+
+    def calculate_tax(self) -> Decimal:
+        return self.price * Decimal(0.05)
+
+    @staticmethod
+    def calculate_shipping_cost(weight: Decimal) -> Decimal:
+        return weight * Decimal(1.5)
+
+    def format_product_name(self) -> str:
+        return f'Discounted Product: {self.name}'
+
+
+class Hero(models.Model, RechargeEnergyMixin):
+    name = models.CharField(
+        max_length=100,
+    )
+
+    hero_title = models.CharField(
+        max_length=100,
+    )
+
+    energy = models.PositiveIntegerField()
+
+
+class SpiderHero(Hero):
+    def swing_from_buildings(self) -> str:
+        if self.energy < 80:
+            return f'{self.name} as Spider Hero is out of web shooter fluid'
+
+        self.energy = max(1, self.energy - 80)
+        self.save()
+
+        return f'{self.name} as Spider Hero swings from buildings using web shooters'
+
+    class Meta:
+        proxy = True
+
+
+class FlashHero(Hero):
+    def run_at_super_speed(self):
+        if self.energy < 65:
+            return f'{self.name} as Flash Hero needs to recharge the speed force'
+
+        self.energy = max(1, self.energy - 65)
+        self.save()
+
+        return f'{self.name} as Flash Hero runs at lightning speed, saving the day'
+
+    class Meta:
+        proxy = True
+
+
+class Document(models.Model):
+    title = models.CharField(
+        max_length=200,
+    )
+
+    content = models.TextField()
+
+    search_vector = SearchVectorField(
+        null=True,
+    )
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['search_vector'])
+        ]
