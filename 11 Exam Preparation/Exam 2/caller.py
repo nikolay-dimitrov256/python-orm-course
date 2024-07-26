09cf5f7efbf8b9784655a4df3f0ce292052c6780
nikolay_dimitrov256 <raystlin7@gmail.com>
2024-07-26 23:38:59 +0300

    Solutions to tasks

--- 11 Exam Preparation/Exam 2/caller.py
+++ 11 Exam Preparation/Exam 2/caller.py
@@ -6,7 +6,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from django.db.models import Q, Count
+from django.db.models import Q, Count, F, Case, When, Value
 from main_app.models import Profile, Product, Order
 
 
@@ -52,3 +52,48 @@ def get_last_sold_products() -> str:
     return f'Last sold products: {", ".join(p.name for p in products)}'
 
 
+def get_top_products() -> str:
+    products = (
+        Product.objects
+        .annotate(sales=Count('order'))
+        .filter(sales__gt=0)
+        .order_by('-sales', 'name')[:5]
+    )
+
+    if not products.exists():
+        return ''
+
+    result = ['Top products:'] + [f'{p.name}, sold {p.sales} times' for p in products]
+
+    return '\n'.join(result)
+
+
+def apply_discounts() -> str:
+    updated_orders = (
+        Order.objects
+        .annotate(products_count=Count('products'))
+        .filter(products_count__gt=2, is_completed=False)
+        .update(total_price=F('total_price') * 0.9)
+    )
+
+    return f'Discount applied to {updated_orders} orders.'
+
+
+def complete_order() -> str:
+    order = Order.objects.filter(is_completed=False).first()
+
+    if not order:
+        return ''
+
+    order.products.update(
+        in_stock=F('in_stock') - 1,
+        is_available=Case(
+            When(in_stock=1, then=Value(False)),
+            default=F('is_available')
+        )
+    )
+
+    order.is_completed = True
+    order.save()
+
+    return 'Order has been completed!'
